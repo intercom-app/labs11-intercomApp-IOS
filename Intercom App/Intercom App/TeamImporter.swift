2da1fb2e576175426b22e90f7a7f6f0da3957fbe
Lotanna <sugabelly@gmail.com>
2019-03-21 14:37:25 -0400

    display list of team members in table view.
    Created datatask to pull list of team member from Back end.
    
    Created table view for displaying list of team members.
    
    Set up cell to display team members downloaded from API.

---
+++ Intercom App/Intercom App/TeamImporter.swift
@@ -0,0 +1,58 @@
+//
+//  TeamImporter.swift
+//  Intercom App
+//
+//  Created by Lotanna Igwe-Odunze on 3/21/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class TeamImporter {
+    
+    static var teamMembers: [User] = []
+    
+    var teamAPIURL = URL(string: "https://intercom-be.herokuapp.com/api/team")
+    
+    
+    func fetchTeam(completion: @escaping (Error?) -> Void) {
+        
+        URLSession.shared.dataTask(with: teamAPIURL!) { (data, urlResponse, error) in
+            
+            if let teamError = error {
+                print("Error getting team members from API: \(teamError)")
+                
+            completion(error)
+                
+            return
+                
+            } //End of error handling.
+            
+            guard let teamData = data else {
+                
+                print("Data was not recieved.")
+                
+                completion(error) //Implement the error message if we fail to get data.
+                return
+            }
+            
+            do {
+                let jsonDecoder = JSONDecoder()
+                
+                let decodedTeam = try jsonDecoder.decode([User].self, from: teamData)
+                
+                TeamImporter.teamMembers = decodedTeam
+                
+                completion(nil)
+            }
+            catch { //In case Decoding doesn't work.
+                
+                NSLog("Error: \(error.localizedDescription)")
+                
+                completion(error) //Show error message in Debugger log.
+                return
+            }
+        } .resume() //Resumes the fetch function if it's been suspended e.g. because of errors.
+        
+    } //End of fetch team function.
+}
