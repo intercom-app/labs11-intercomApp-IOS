1eb4627fb287ccff21023012788c9c123457bf49
Sergey Osipyan <osipyansergey@gmail.com>
2019-03-21 14:57:21 -0700

    added Login view with facebook and twitter buttons, added userName an password text fields. also added shape, color and design, DB is down for this moment

---
+++ Intercom App/Intercom App/Interface/LoginButton.swift
@@ -0,0 +1,30 @@
+//
+//  LoginButton.swift
+//  Intercom App
+//
+//  Created by Sergey Osipyan on 3/21/19.
+//  Copyright © 2019 Sergey Osipyan. All rights reserved.
+//
+
+
+import UIKit
+
+let kLoginButtonBackgroundColor = UIColor(displayP3Red: 31/255, green: 75/255, blue: 164/255, alpha: 1)
+let kLoginButtonTintColor = UIColor.white
+let kLoginButtonCornerRadius: CGFloat = 13.0
+
+class LoginButton: UIButton {
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.configureUI()
+    }
+
+    private func configureUI() {
+        self.backgroundColor = kLoginButtonBackgroundColor
+        self.layer.cornerRadius = kLoginButtonCornerRadius
+        self.tintColor = kLoginButtonTintColor
+        self.titleLabel?.font = UIFont(name: "AppleSDGothicNeo-Regular", size: 14)
+    }
+
+}
