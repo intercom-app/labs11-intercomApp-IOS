ef61f73b3f85709771130cb878fa1dc7b159d568
Sergey Osipyan <osipyansergey@gmail.com>
2019-03-26 16:00:37 -0700

    added basic objects to chat room and add some func to swit file

--- Intercom App/Intercom App/View Controllers/Login/GroupChatRoomViewController.swift
+++ Intercom App/Intercom App/View Controllers/Login/GroupChatRoomViewController.swift
@@ -7,14 +7,92 @@
 //
 
 import UIKit
+import AVFoundation
+import PushKit
 import TwilioVoice
 
-class GroupChatRoomViewController: UIViewController {
+let baseURLString = "https://3b57e324.ngrok.io"
+let accessTokenEndpoint = "/accessToken"
+let identity = "alice"
+let twimlParamTo = "to"
+
+class GroupChatRoomViewController: UIViewController, PKPushRegistryDelegate, TVONotificationDelegate, TVOCallDelegate, AVAudioPlayerDelegate, UITextFieldDelegate {
+    
+    
+   
+    @IBOutlet weak var placeCallButton: UIButton!
+    @IBOutlet weak var groupImage: UIImageView!
+    //@IBOutlet weak var outgoingValue: UITextField!
+    @IBOutlet weak var callControlView: UIView!
+    @IBOutlet weak var muteSwitch: UISwitch!
+    @IBOutlet weak var speakerSwitch: UISwitch!
+    
+    var deviceTokenString:String?
+    var voipRegistry:PKPushRegistry
+    var incomingPushCompletionCallback: (()->Swift.Void?)? = nil
+    var incomingAlertController: UIAlertController?
+    var callInvite:TVOCallInvite?
+    var call:TVOCall?
+    var ringtonePlayer:AVAudioPlayer?
+    var ringtonePlaybackCallback: (() -> ())?
+    
+    required init?(coder aDecoder: NSCoder) {
+        voipRegistry = PKPushRegistry.init(queue: DispatchQueue.main)
+        super.init(coder: aDecoder)
+        voipRegistry.delegate = self
+        voipRegistry.desiredPushTypes = Set([PKPushType.voIP])
+        TwilioVoice.logLevel = .verbose
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
+    }
+    
+    func fetchAccessToken() -> String? {
+        let endpointWithIdentity = String(format: "%@?identity=%@", accessTokenEndpoint, identity)
+        guard let accessTokenURL = URL(string: baseURLString + endpointWithIdentity) else {
+            return nil
+        }
+        
+        return try? String.init(contentsOf: accessTokenURL, encoding: .utf8)
+    }
+    
+    func toggleUIState(isEnabled: Bool, showCallControl: Bool) {
+        placeCallButton.isEnabled = isEnabled
+        if (showCallControl) {
+            callControlView.isHidden = false
+            muteSwitch.isOn = false
+            speakerSwitch.isOn = true
+        } else {
+            callControlView.isHidden = true
+        }
+    }
+    
+    
+    func pushRegistry(_ registry: PKPushRegistry, didUpdate pushCredentials: PKPushCredentials, for type: PKPushType) {
+        <#code#>
+    }
+    
+    func callInviteReceived(_ callInvite: TVOCallInvite) {
+        <#code#>
+    }
+    
+    func notificationError(_ error: Error) {
+        <#code#>
+    }
+    
+    func callDidConnect(_ call: TVOCall) {
+        <#code#>
+    }
+    
+    func call(_ call: TVOCall, didFailToConnectWithError error: Error) {
+        <#code#>
+    }
+    
+    func call(_ call: TVOCall, didDisconnectWithError error: Error?) {
+        <#code#>
     }
     
 
