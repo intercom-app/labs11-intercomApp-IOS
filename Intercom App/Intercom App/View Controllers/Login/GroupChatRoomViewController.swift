7b49425b4637b30abc445d875f59a3d7346503bc
Sergey Osipyan <osipyansergey@gmail.com>
2019-03-27 08:51:06 -0700

    added some func to Twilio but need to wait until BE will setup server, also updated to xCode 10.2 (works fine with CocoaPads)

--- Intercom App/Intercom App/View Controllers/Login/GroupChatRoomViewController.swift
+++ Intercom App/Intercom App/View Controllers/Login/GroupChatRoomViewController.swift
@@ -11,7 +11,7 @@ import AVFoundation
 import PushKit
 import TwilioVoice
 
-let baseURLString = "https://3b57e324.ngrok.io"
+let baseURLString = "http://649579d2.ngrok.io"  // http://649579d2.ngrok.io/api/voice
 let accessTokenEndpoint = "/accessToken"
 let identity = "alice"
 let twimlParamTo = "to"
@@ -22,7 +22,7 @@ class GroupChatRoomViewController: UIViewController, PKPushRegistryDelegate, TVO
    
     @IBOutlet weak var placeCallButton: UIButton!
     @IBOutlet weak var groupImage: UIImageView!
-    //@IBOutlet weak var outgoingValue: UITextField!
+    @IBOutlet weak var outgoingValue: UITextField!
     @IBOutlet weak var callControlView: UIView!
     @IBOutlet weak var muteSwitch: UISwitch!
     @IBOutlet weak var speakerSwitch: UISwitch!
@@ -36,6 +36,8 @@ class GroupChatRoomViewController: UIViewController, PKPushRegistryDelegate, TVO
     var ringtonePlayer:AVAudioPlayer?
     var ringtonePlaybackCallback: (() -> ())?
     
+    
+    
     required init?(coder aDecoder: NSCoder) {
         voipRegistry = PKPushRegistry.init(queue: DispatchQueue.main)
         super.init(coder: aDecoder)
@@ -47,7 +49,8 @@ class GroupChatRoomViewController: UIViewController, PKPushRegistryDelegate, TVO
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        toggleUIState(isEnabled: true, showCallControl: false)
+        outgoingValue.delegate = self
     }
     
     func fetchAccessToken() -> String? {
@@ -70,29 +73,358 @@ class GroupChatRoomViewController: UIViewController, PKPushRegistryDelegate, TVO
         }
     }
     
-    
-    func pushRegistry(_ registry: PKPushRegistry, didUpdate pushCredentials: PKPushCredentials, for type: PKPushType) {
-        <#code#>
+    @IBAction func placeCall(_ sender: UIButton) {
+        if (self.call != nil) {
+            self.call?.disconnect()
+            self.toggleUIState(isEnabled: false, showCallControl: false)
+        } else {
+            guard let accessToken = fetchAccessToken() else {
+                return
+            }
+            
+            playOutgoingRingtone(completion: { [weak self] in
+                if let strongSelf = self {
+                    strongSelf.call = TwilioVoice.call(accessToken, params: [twimlParamTo : strongSelf.outgoingValue.text!], delegate: strongSelf)
+                    strongSelf.toggleUIState(isEnabled: false, showCallControl: false)
+                    
+                }
+            })
+        }
     }
     
+    @IBAction func muteSwitchToggled(_ sender: UISwitch) {
+        if let call = call {
+            call.isMuted = sender.isOn
+        } else {
+            NSLog("No active call to be muted")
+        }
+    }
+    
+    @IBAction func speakerSwitchToggled(_ sender: UISwitch) {
+        toggleAudioRoute(toSpeaker: sender.isOn)
+    }
+    
+    // MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        outgoingValue.resignFirstResponder()
+        return true
+    }
+    
+    // MARK: PKPushRegistryDelegate
+    func pushRegistry(_ registry: PKPushRegistry, didUpdate credentials: PKPushCredentials, for type: PKPushType) {
+        NSLog("pushRegistry:didUpdatePushCredentials:forType:");
+        
+        if (type != .voIP) {
+            return
+        }
+        
+        guard let accessToken = fetchAccessToken() else {
+            return
+        }
+        
+        let deviceToken = (credentials.token as NSData).description
+        
+        TwilioVoice.register(withAccessToken: accessToken, deviceToken: deviceToken) { (error) in
+            if let error = error {
+                NSLog("An error occurred while registering: \(error.localizedDescription)")
+            }
+            else {
+                NSLog("Successfully registered for VoIP push notifications.")
+            }
+        }
+        
+        self.deviceTokenString = deviceToken
+    }
+    
+    func pushRegistry(_ registry: PKPushRegistry, didInvalidatePushTokenFor type: PKPushType) {
+        NSLog("pushRegistry:didInvalidatePushTokenForType:")
+        
+        if (type != .voIP) {
+            return
+        }
+        
+        guard let deviceToken = deviceTokenString, let accessToken = fetchAccessToken() else {
+            return
+        }
+        
+        TwilioVoice.unregister(withAccessToken: accessToken, deviceToken: deviceToken) { (error) in
+            if let error = error {
+                NSLog("An error occurred while unregistering: \(error.localizedDescription)")
+            }
+            else {
+                NSLog("Successfully unregistered from VoIP push notifications.")
+            }
+        }
+        
+        self.deviceTokenString = nil
+    }
+    
+    
+    func pushRegistry(_ registry: PKPushRegistry, didReceiveIncomingPushWith payload: PKPushPayload, for type: PKPushType, completion: @escaping () -> Void) {
+        NSLog("pushRegistry:didReceiveIncomingPushWithPayload:forType:completion:")
+        
+        // Save for later when the notification is properly handled.
+        self.incomingPushCompletionCallback = completion;
+        
+        if (type == PKPushType.voIP) {
+            TwilioVoice.handleNotification(payload.dictionaryPayload, delegate: self)
+        }
+    }
+    
+    func incomingPushHandled() {
+        if let completion = self.incomingPushCompletionCallback {
+            completion()
+            self.incomingPushCompletionCallback = nil
+        }
+    }
+    
+    // MARK: TVONotificaitonDelegate
     func callInviteReceived(_ callInvite: TVOCallInvite) {
-        <#code#>
+        if (callInvite.state == .pending) {
+            handleCallInviteReceived(callInvite)
+        } else if (callInvite.state == .canceled) {
+            handleCallInviteCanceled(callInvite)
+        }
+    }
+    
+    func handleCallInviteReceived(_ callInvite: TVOCallInvite) {
+        NSLog("callInviteReceived:")
+        
+        if (self.callInvite != nil && self.callInvite?.state == .pending) {
+            NSLog("Already a pending call invite. Ignoring incoming call invite from \(callInvite.from)")
+            self.incomingPushHandled()
+            return
+        } else if (self.call != nil && self.call?.state == .connected) {
+            NSLog("Already an active call. Ignoring incoming call invite from \(callInvite.from)");
+            self.incomingPushHandled()
+            return;
+        }
+        
+        self.callInvite = callInvite;
+        
+        let from = callInvite.from
+        let alertMessage = "From: \(from)"
+        
+        playIncomingRingtone()
+        
+        let incomingAlertController = UIAlertController(title: "Incoming",
+                                                        message: alertMessage,
+                                                        preferredStyle: .alert)
+        
+        let rejectAction = UIAlertAction(title: "Reject", style: .default) { [weak self] (action) in
+            if let strongSelf = self {
+                strongSelf.stopIncomingRingtone()
+                callInvite.reject()
+                strongSelf.callInvite = nil
+                
+                strongSelf.incomingAlertController = nil
+                strongSelf.toggleUIState(isEnabled: true, showCallControl: false)
+            }
+        }
+        incomingAlertController.addAction(rejectAction)
+        
+        let ignoreAction = UIAlertAction(title: "Ignore", style: .default) { [weak self] (action) in
+            if let strongSelf = self {
+                /* To ignore the call invite, you don't have to do anything but just literally ignore it */
+                
+                strongSelf.callInvite = nil
+                strongSelf.stopIncomingRingtone()
+                strongSelf.incomingAlertController = nil
+                strongSelf.toggleUIState(isEnabled: true, showCallControl: false)
+            }
+        }
+        incomingAlertController.addAction(ignoreAction)
+        
+        let acceptAction = UIAlertAction(title: "Accept", style: .default) { [weak self] (action) in
+            if let strongSelf = self {
+                strongSelf.stopIncomingRingtone()
+                strongSelf.call = callInvite.accept(with: strongSelf)
+                strongSelf.callInvite = nil
+                
+                strongSelf.incomingAlertController = nil
+                
+            }
+        }
+        incomingAlertController.addAction(acceptAction)
+        
+        toggleUIState(isEnabled: false, showCallControl: false)
+        present(incomingAlertController, animated: true, completion: nil)
+        self.incomingAlertController = incomingAlertController
+        
+        // If the application is not in the foreground, post a local notification
+        if (UIApplication.shared.applicationState != UIApplication.State.active) {
+            let notification = UILocalNotification()
+            notification.alertBody = "Incoming Call From \(from)"
+            
+            UIApplication.shared.presentLocalNotificationNow(notification)
+        }
+        
+        self.incomingPushHandled()
+    }
+    
+    func handleCallInviteCanceled(_ callInvite: TVOCallInvite) {
+        NSLog("callInviteCanceled:")
+        
+        if (callInvite.callSid != self.callInvite?.callSid) {
+            NSLog("Incoming (but not current) call invite from \(callInvite.from) canceled. Just ignore it.");
+            return;
+        }
+        
+        self.stopIncomingRingtone()
+        playDisconnectSound()
+        
+        if (incomingAlertController != nil) {
+            dismiss(animated: true) { [weak self] in
+                if let strongSelf = self {
+                    strongSelf.incomingAlertController = nil
+                    strongSelf.toggleUIState(isEnabled: true, showCallControl: false)
+                }
+            }
+        }
+        
+        self.callInvite = nil
+        
+        UIApplication.shared.cancelAllLocalNotifications()
+        
+        self.incomingPushHandled()
     }
     
     func notificationError(_ error: Error) {
-        <#code#>
+        NSLog("notificationError: \(error.localizedDescription)")
     }
     
+    
+    // MARK: TVOCallDelegate
     func callDidConnect(_ call: TVOCall) {
-        <#code#>
+        NSLog("callDidConnect:")
+        
+        self.call = call
+        
+        self.placeCallButton.setTitle("Hang Up", for: .normal)
+        
+        toggleUIState(isEnabled: true, showCallControl: true)
+        toggleAudioRoute(toSpeaker: true)
     }
     
     func call(_ call: TVOCall, didFailToConnectWithError error: Error) {
-        <#code#>
+        NSLog("Call failed to connect: \(error.localizedDescription)")
+        
+        callDisconnected()
     }
     
     func call(_ call: TVOCall, didDisconnectWithError error: Error?) {
-        <#code#>
+        if let error = error {
+            NSLog("Call failed: \(error.localizedDescription)")
+        } else {
+            NSLog("Call disconnected")
+        }
+        
+        callDisconnected()
+    }
+    
+    func callDisconnected() {
+        self.call = nil
+        
+        playDisconnectSound()
+        toggleUIState(isEnabled: true, showCallControl: false)
+        self.placeCallButton.setTitle("Call", for: .normal)
+    }
+    
+    
+    // MARK: AVAudioSession
+    func toggleAudioRoute(toSpeaker: Bool) {
+        // The mode set by the Voice SDK is "VoiceChat" so the default audio route is the built-in receiver. Use port override to switch the route.
+        do {
+            if (toSpeaker) {
+                try AVAudioSession.sharedInstance().overrideOutputAudioPort(.speaker)
+            } else {
+                try AVAudioSession.sharedInstance().overrideOutputAudioPort(.none)
+            }
+        } catch {
+            NSLog(error.localizedDescription)
+        }
+    }
+    
+    
+    // MARK: Ringtone player & AVAudioPlayerDelegate
+    func playOutgoingRingtone(completion: @escaping () -> ()) {
+        self.ringtonePlaybackCallback = completion
+        
+        let ringtonePath = URL(fileURLWithPath: Bundle.main.path(forResource: "outgoing", ofType: "wav")!)
+        do {
+            self.ringtonePlayer = try AVAudioPlayer(contentsOf: ringtonePath)
+            self.ringtonePlayer?.delegate = self
+            
+            playRingtone()
+        } catch {
+            NSLog("Failed to initialize audio player")
+            self.ringtonePlaybackCallback?()
+        }
+    }
+    
+    func playIncomingRingtone() {
+        let ringtonePath = URL(fileURLWithPath: Bundle.main.path(forResource: "incoming", ofType: "wav")!)
+        do {
+            self.ringtonePlayer = try AVAudioPlayer(contentsOf: ringtonePath)
+            self.ringtonePlayer?.delegate = self
+            self.ringtonePlayer?.numberOfLoops = -1
+            
+            playRingtone()
+        } catch {
+            NSLog("Failed to initialize audio player")
+        }
+    }
+    
+    func stopIncomingRingtone() {
+        if (self.ringtonePlayer?.isPlaying == false) {
+            return
+        }
+        
+        self.ringtonePlayer?.stop()
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playAndRecord)
+        } catch {
+            NSLog(error.localizedDescription)
+        }
+    }
+    
+    func playDisconnectSound() {
+        let ringtonePath = URL(fileURLWithPath: Bundle.main.path(forResource: "disconnect", ofType: "wav")!)
+        do {
+            self.ringtonePlayer = try AVAudioPlayer(contentsOf: ringtonePath)
+            self.ringtonePlayer?.delegate = self
+            self.ringtonePlaybackCallback = nil
+            
+            playRingtone()
+        } catch {
+            NSLog("Failed to initialize audio player")
+        }
+    }
+    
+    func playRingtone() {
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback)
+        } catch {
+            NSLog(error.localizedDescription)
+        }
+        
+        self.ringtonePlayer?.volume = 1.0
+        self.ringtonePlayer?.play()
+    }
+    
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        if (self.ringtonePlaybackCallback != nil) {
+            DispatchQueue.main.async {
+                self.ringtonePlaybackCallback!()
+            }
+        }
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playAndRecord)
+        } catch {
+            NSLog(error.localizedDescription)
+        }
     }
     
 
