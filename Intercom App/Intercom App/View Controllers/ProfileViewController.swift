aece738894175e11849e0ddbfc3c15feee4949d0
Lotanna <sugabelly@gmail.com>
2019-03-27 19:57:04 -0400

    created Profile page and view

---
+++ Intercom App/Intercom App/View Controllers/ProfileViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ProfileViewController.swift
+//  Intercom App
+//
+//  Created by Lotanna Igwe-Odunze on 3/27/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+    @IBOutlet weak var photoView: UIImageView!
+    
+    @IBOutlet weak var fullnameLabel: UILabel!
+    
+    @IBOutlet weak var phoneLabel: UILabel!
+    
+    @IBOutlet weak var callStatusLabel: UILabel!
+    
+    @IBOutlet weak var emailLabel: UILabel!
+    
+    @IBOutlet weak var creationDateLabel: UILabel!
+    
+    @IBOutlet weak var subscriptionLabel: UILabel!
+    
+    
+    
+}
