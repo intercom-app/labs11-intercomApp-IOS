8b558dde7f0ae41d57d67b668357ac017e391284
Sergey Osipyan <osipyansergey@gmail.com>
2019-04-02 13:39:25 -0700

    added POST request for Group controller and added alert when you add text and add new group

--- Intercom App/Intercom App/Importers/GroupController.swift
+++ Intercom App/Intercom App/Importers/GroupController.swift
@@ -71,5 +71,52 @@ class GroupController {
     } //End of fetch team function.
     
     
-   
+    func postRequest(groupName: String) {
+        
+        //declare parameter as a dictionary which contains string as key and value combination. considering inputs are valid
+        
+        let parameters = ["name": groupName]
+        
+        //create the session object
+        let session = URLSession.shared
+        
+        //now create the URLRequest object using the url object
+        var request = URLRequest(url: groupBaseURL)
+        request.httpMethod = "POST" //set http method as POST
+        
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted) // pass dictionary to nsdata object and set it as request body
+        } catch let error {
+            print(error.localizedDescription)
+        }
+        
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        
+        //create dataTask using the session object to send data to the server
+        let task = session.dataTask(with: request as URLRequest, completionHandler: { data, response, error in
+            
+            guard error == nil else {
+                return
+            }
+            
+            guard let data = data else {
+                return
+            }
+            self.fetchGroups()
+            
+            do {
+                //create json object from data
+                if let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] {
+                    print(json)
+                    // handle json...
+                  
+                }
+            } catch let error {
+                print(error.localizedDescription)
+            }
+           
+        })
+        task.resume()
+    }
 }
