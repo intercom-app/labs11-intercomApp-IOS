3e032101c2a69b3d538c0545af7cb9047d9491cb
Lotanna <sugabelly@gmail.com>
2019-04-12 10:57:19 -0400

    created billing view controller
    added connections for billing UI

---
+++ Intercom App/Intercom App/View Controllers/BillingViewController.swift
@@ -0,0 +1,41 @@
+//
+//  BillingViewController.swift
+//  Intercom App
+//
+//  Created by Lotanna Igwe-Odunze on 4/12/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class BillingViewController: UIViewController {
+    
+    //Outlets
+    @IBOutlet weak var creditLabel: UILabel!
+    @IBOutlet weak var cardInfoLabel: UILabel!
+    @IBOutlet weak var cardIconView: UIImageView!
+    @IBOutlet weak var cardNameField: UITextField!
+    @IBOutlet weak var cardNumberField: UITextField!
+    @IBOutlet weak var cardDateField: UITextField!
+    @IBOutlet weak var cardCVCField: UITextField!
+    @IBOutlet weak var updatePaymentButton: UIButton!
+    
+    //Properties
+    
+    
+    //Functions
+    
+    
+    //Actions
+    
+    @IBAction func updatePaymentClicked(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func saveClicked(_ sender: UIBarButtonItem) {
+    }
+    
+    
+    
+    
+}
