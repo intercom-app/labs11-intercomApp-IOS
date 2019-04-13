cde4e61f241d5f10ee775d778e29a64657a5e78a
Sergey Osipyan <osipyansergey@gmail.com>
2019-04-12 22:54:43 -0700

    added func to join group button for invitation, also after join sending activity log

--- Intercom App/Intercom App/View Controllers/Cell View/GroupViewTableViewCell.swift
+++ Intercom App/Intercom App/View Controllers/Cell View/GroupViewTableViewCell.swift
@@ -20,12 +20,17 @@ class GroupViewTableViewCell: UITableViewCell {
     @IBOutlet weak var joinGroupbutton: UIButton!
     
     @IBAction func joinGroupButtonAction(_ sender: Any) {
-        
+        guard let groupId = groupId else { return }
+         GroupController.shared.deleteInvitation(groupID: groupId, userID: TeamImporter.shared.userID)
+         GroupController.shared.addGroupMember(groupID: groupId)
+         GroupController.shared.postActivity(groupID: groupId, massage: "Joined group.")
+         TeamImporter.shared.fetchCurentUserDetails(userID: TeamImporter.shared.userID!)
     }
     
     @IBAction func declineButtonAction(_ sender: Any) {
-        GroupController.shared.deleteInvitation(groupID: groupId!, userID: TeamImporter.shared.userID)
-        GroupController.shared.postActivity(groupID: groupId!, massage: "Declined to join the group")
+        guard let groupId = groupId else { return }
+        GroupController.shared.deleteInvitation(groupID: groupId, userID: TeamImporter.shared.userID)
+        GroupController.shared.postActivity(groupID: groupId, massage: "Declined to join the group")
         TeamImporter.shared.fetchCurentUserDetails(userID: TeamImporter.shared.userID!)
     }
     override func setSelected(_ selected: Bool, animated: Bool) {
