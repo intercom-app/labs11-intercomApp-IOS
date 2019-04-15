33cc264fcb8c3abef28841bc158e645a3c5e6dbb
Sergey Osipyan <osipyansergey@gmail.com>
2019-04-15 12:03:28 -0700

    added part Code for conference call notification, added helper files and modify appDelegate

--- Intercom App/Intercom App/Resources/AppDelegate.swift
+++ Intercom App/Intercom App/Resources/AppDelegate.swift
@@ -14,14 +14,51 @@ import UserNotifications
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-  
+    var devToken: String?
+    var environment: APNSEnvironment?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         NSLog("Twilio Voice Version: %@", TwilioVoice.version())
         self.configureUserNotifications()
         
+        let settings = UIUserNotificationSettings(types: .alert, categories: nil)
+        UIApplication.shared.registerUserNotificationSettings(settings)
+        UIApplication.shared.registerForRemoteNotifications()
+       
+        // Override point for customization after application launch.
+        if #available(iOS 10, *) {
+            let center = UNUserNotificationCenter.current()
+            center.requestAuthorization(options: [.alert, .badge, .sound], completionHandler: { (granted, error) in
+                DispatchQueue.main.async {
+                    UIApplication.shared.registerForRemoteNotifications()
+                }
+            })
+        } else {
+            let settings = UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+            UIApplication.shared.registerUserNotificationSettings(settings)
+            UIApplication.shared.registerForRemoteNotifications()
+        }
+        
+        self.environment = ProvisioningProfileInspector().apnsEnvironment()
+        var envString = "Unknown"
+        if environment != APNSEnvironment.unknown {
+            if environment == APNSEnvironment.development {
+                envString = "Development"
+            } else {
+                envString = "Production"
+            }
+        }
+        print("APNS Environment detected as: \(envString) ");
         return true
     }
+    
+    
+    
+    func application(application: UIApplication, didReceiveRemoteNotification userInfo: [NSObject : AnyObject]) {
+        print("Recived: \(userInfo)")
+        //Parsing userinfo:
+    }
+   
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
@@ -43,6 +80,32 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        let tokenChars = (deviceToken as NSData).bytes.bindMemory(to: CChar.self, capacity: deviceToken.count)
+        var tokenString = ""
+        for i in 0..<deviceToken.count {
+            tokenString += String(format: "%02.2hhx", arguments: [tokenChars[i]])
+        }
+        print("Received token data! \(tokenString)")
+        devToken = tokenString
+    }
+    
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("Couldn't register: \(error)")
+    }
+    
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any]) {
+        print("Message received.")
+        if let aps = userInfo[AnyHashable("aps")] as? [AnyHashable: Any] {
+            if let alert = aps[AnyHashable("alert")] as? String {
+                let alertController = UIAlertController(title: "Incoming Notification", message: alert, preferredStyle: .alert)
+                let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+                alertController.addAction(defaultAction)
+                self.window?.rootViewController?.present(alertController, animated: true, completion: nil)
+            }
+        }
     }
 
     func configureUserNotifications() {
