51650ad00b37d34957bc1eb196adb4f2c8cdc098
Sergey Osipyan <osipyansergey@gmail.com>
2019-04-17 13:44:10 -0700

    added badge to invites cell, showng group ivites count

--- Intercom App/Intercom App/View Controllers/GroupListViewController.swift
+++ Intercom App/Intercom App/View Controllers/GroupListViewController.swift
@@ -85,7 +85,11 @@ class GroupListViewController: UITableViewController {
             
             cell.groupId = group.groupID
             cell.groupOwnedToNameLabel.text = "Invites"
-            //cell.groupOwnedTonumberOfUsers.text = "\(group.members.count) users"
+            
+            // Configure Cell Badge
+            if let badgeCount = TeamImporter.shared.allGroups?[2].count {
+           cell.update(count: badgeCount)
+            }
             return cell
         } else {
             
@@ -98,7 +102,7 @@ class GroupListViewController: UITableViewController {
         
     }
     override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 75
+        return 70
     }
     
     override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
@@ -135,10 +139,6 @@ class GroupListViewController: UITableViewController {
         
     }
     
-    //
-    
-    
-    
     override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
         
         if tableView.indexPathForSelectedRow?.section == 2 {
@@ -170,4 +170,41 @@ class GroupListViewController: UITableViewController {
         }
     }
     
+}
+
+extension UITableViewCell {
+    func update(count: Int) {
+        // Count > 0, show count
+        if count > 0 {
+            
+            // Create label
+            let fontSize: CGFloat = 16
+            let label = UILabel()
+            label.font = UIFont.systemFont(ofSize: fontSize)
+            label.textAlignment = .center
+            label.textColor = UIColor.white
+            label.backgroundColor = UIColor(displayP3Red: 0/255, green: 122/255, blue: 255/255, alpha: 1)
+            
+            // Add count to label and size to fit
+            label.text = "\(NSNumber(value: count))"
+            label.sizeToFit()
+            
+            // Adjust frame to be square for single digits or elliptical for numbers > 9
+            var frame: CGRect = label.frame
+            frame.size.height += CGFloat(Int(0.4 * fontSize))
+            frame.size.width = (count <= 9) ? frame.size.height : frame.size.width + CGFloat(Int(fontSize))
+            label.frame = frame
+            
+            // Set radius and clip to bounds
+            label.layer.cornerRadius = frame.size.height / 2.0
+            label.clipsToBounds = true
+            
+            // Show label in accessory view and remove disclosure
+            self.accessoryView = label
+            self.accessoryType = .none
+        } else {
+            self.accessoryView = nil
+            self.accessoryType = .disclosureIndicator
+        }
+    }
 }
